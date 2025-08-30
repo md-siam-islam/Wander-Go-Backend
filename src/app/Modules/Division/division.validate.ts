0ec2a15cf9071b64cae90658b7e8455f31af741e
md-siam-islam <mdsiamislam663@gmail.com>
2025-08-31 05:50:17 +0600

    Division Zood validation File create Done

---
+++ src/app/Modules/Division/division.validate.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+
+export const createDivisionZoodSchema = z.object({
+    name : z.string({
+        required_error : "Division name is required",
+        invalid_type_error : "Division name must be a string"
+    }).min(2 , {message : "Division name must be at least 2 characters"})
+    .max(50, {message : "Division name must be at most 50 characters"}),
+
+    slug : z.string({
+        invalid_type_error : "Division slug must be a string"
+    }).optional(),
+
+    thumbnail : z.string({
+        invalid_type_error : "Division thumbnail must be a string"
+    }).optional(),
+
+    description : z.string({
+        invalid_type_error : "Division description must be a string"
+    }).optional()
+})
