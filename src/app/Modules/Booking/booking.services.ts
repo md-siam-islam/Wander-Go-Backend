53311361558cdfa6f10a6c54371a0c6aaa3ddfb1
md-siam-islam <mdsiamislam663@gmail.com>
2025-09-12 21:02:18 +0600

    Bookins session Transiction create done

--- src/app/Modules/Booking/booking.services.ts
+++ src/app/Modules/Booking/booking.services.ts
@@ -12,9 +12,12 @@ import { randomUUID } from "crypto";
 // };
 
 const createBooking = async (payload:Partial<IBooking> , userId : string) => {
-
     // const transactionId = getTransactionId()
 
+    const session = await Booking.startSession()
+    session.startTransaction()
+
+    try {
     const user = await User.findById(userId)
 
     if(!user){
@@ -33,22 +36,32 @@ const createBooking = async (payload:Partial<IBooking> , userId : string) => {
 
     const TotalAmount = Number(tour.costFrom) * Number(payload.guestCount)
 
-    const booking =await Booking.create({
+    const booking =await Booking.create([{
         user : userId,
         BookingStatus : BOOKING_ENUM.PENDING,
         ...payload
-    })
+    }],{session})
 
-    const payment = await Payment.create({
-        booking : booking._id,
+    const payment = await Payment.create([{
+        booking : booking[0]._id,
         status : PAYMENT_STATUS.UNPAID,
         // TransactionId : transactionId,
         amount : TotalAmount
-    })
+    }] ,{session})
 
-    const UpdatedBooking = await Booking.findByIdAndUpdate(booking._id , {payment :  payment._id} , {new : true , runValidators : true})
+    const UpdatedBooking = await Booking.findByIdAndUpdate(booking[0]._id , {payment :  payment[0]._id} , {new : true , runValidators : true, session}).populate("user" , "name email phone address",).populate("tour" , "title costFrom").populate("payment")
 
+    await session.commitTransaction()
+    session.endSession()
     return UpdatedBooking
+
+    } catch (error) {
+        await session.abortTransaction()
+        session.endSession()
+        throw error
+    }
+
+    
 }
 
  export const BookingServices = {
